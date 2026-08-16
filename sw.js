88373cfde0b53e65367e92f928d5fb4d19db7641
hr-council-hyd <hr.councilhyd@gmail.com>
2026-08-16 10:06:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,57 @@
 
-var CACHE_NAME = 'didar2026-v1';
+var CACHE_NAME = 'didar2026-v2';
 var urlsToCache = [
   './',
   './index.html',
   './manifest.json'
 ];
 
+// Install - cache basic files
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).then(function(response) {
-      var responseClone = response.clone();
-      caches.open(CACHE_NAME).then(function(cache) {
-        cache.put(event.request, responseClone);
-      });
-      return response;
-    }).catch(function() {
-      return caches.match(event.request);
-    })
-  );
-});
-
+// Activate - clean old caches
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function(names) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
+        names.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
       );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - IMPORTANT: Let API calls pass through without caching
+self.addEventListener('fetch', function(event) {
+  var url = event.request.url;
+
+  // DO NOT intercept Google Apps Script API calls
+  if (url.indexOf('script.google.com') !== -1) {
+    return;
+  }
+
+  // DO NOT intercept Google Drive calls
+  if (url.indexOf('drive.google.com') !== -1) {
+    return;
+  }
+
+  // DO NOT intercept POST requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  // For everything else, try cache first, then network
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
